a34d76233cbcffedd2bc431ec5b4d3da2e8ec77b
ivoschugurensky <107146836+ivoschugurensky@users.noreply.github.com>
2022-11-08 08:52:18 -0300

    Commit 7
    Ahora 2 gráficos

--- calculadora-fisica.py
+++ calculadora-fisica.py
@@ -1,9 +1,13 @@
+#Correr este programa en visual studio code con el matplotlib instalado por terminal
+#Usar pip install matplotlib
+#Si el auto no llega a chocar ninguna de las veces que se repita el código se mostrará otro gráfico de la Distancia del caballo sobre tiempo
 import matplotlib.pyplot as plt
 
 repeticion=int(input("Cuantas veces queres que se repita el codigo: "))
 lista=[]
 x=[]
 y=[]
+x2=[]
 for i in range(0,repeticion):
     if repeticion == 1:
       singrafico = input("¿Seguro que quiere repetirlo una vez? Si es así no va a haber gráfico (si/no):")
@@ -49,17 +53,29 @@ for i in range(0,repeticion):
             print("Frenaste en ",tie, "segundos")
             lista.append(dist2)
             lista.append(tie)
+            x2.append(dist2)
         y.append(tie)
         x.append(dist)
     variables()
 
 if repeticion > 1:
   print("Los resultados fueron",lista)
-  width=0.1
+  plt.figure(1)
+  width=0.05
   plt.bar(y,x, width=width, color="brown")
   plt.scatter(y,x, color="grey")
   plt.plot(y,x, color="black")
   plt.ylabel("Distancia en metros")
   plt.xlabel("Tiempo en segundos")
   plt.title("Distancia sobre tiempo")
+  plt.show()
+  
+  plt.figure(2)
+  width=0.1
+  plt.bar(y,x2, width=width, color="brown")
+  plt.scatter(y,x2, color="grey")
+  plt.plot(y,x2, color="black")
+  plt.ylabel("Distancia del caballo (en metros)")
+  plt.xlabel("Tiempo en segundos")
+  plt.title("Distancia del caballo sobre tiempo")
   plt.show()
